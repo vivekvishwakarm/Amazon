1a39dacc1d1427aee41e03a08747487185cc84d4
Vivek Vishwakarma <vivek6306619@gmail.com>
2024-09-13 23:27:57 +0530

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:amazon/utils/theme.dart';
+import 'package:amazon/view/auth_screen/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Amazon());
+}
+
+class Amazon extends StatelessWidget {
+  const Amazon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      title: 'Amazon',
+      debugShowCheckedModeBanner: false,
+      home: const AuthScreen(),
+    );
+  }
+}
---
+++ lib/view/auth_screen/auth_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Image(
+          image: const AssetImage(
+            'assets/images/amazon_logo.png',
+          ),
+          height: height * 0.04,
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.03,
+            vertical: height * 0.02,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome",
+                style: textTheme.bodyLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
