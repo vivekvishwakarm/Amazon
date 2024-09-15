1c802588be6249d28b4441924e166d8498394e72
Vivek Vishwakarma <vivek6306619@gmail.com>
2024-09-16 00:55:30 +0530

    Day-1 task completed

--- lib/view/auth_screen/auth_screen.dart
+++ lib/view/auth_screen/auth_screen.dart
@@ -1,4 +1,7 @@
+import 'package:amazon/utils/colors.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:amazon/constants/common_functions.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -8,12 +11,15 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  bool isLogin = false;
+  String currentCountryCode = "+91";
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
+      backgroundColor: white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Image(
@@ -37,8 +43,170 @@ class _AuthScreenState extends State<AuthScreen> {
             children: [
               Text(
                 "Welcome",
-                style: textTheme.bodyLarge!.copyWith(
-                  fontWeight: FontWeight.bold,
+                style: textTheme.displayLarge!.copyWith(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              CommonFunctions.blankSpace(height * 0.02, 0),
+              Container(
+                width: width,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: greyShade3,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: height * 0.06,
+                      width: width,
+                      decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: greyShade3,
+                            ),
+                          ),
+                          color: greyShade1),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.03,
+                      ),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                isLogin = true;
+                              });
+                            },
+                            child: Container(
+                              height: height * 0.03,
+                              width: height * 0.03,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(color: grey),
+                                  color: white),
+                              child: Icon(
+                                Icons.circle,
+                                size: height * 0.015,
+                                color: isLogin ? transparent : secondaryColor,
+                              ),
+                            ),
+                          ),
+                          CommonFunctions.blankSpace(
+                            0,
+                            width * 0.02,
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "Create Account. ",
+                                  style: textTheme.bodyMedium!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: "New to amazon?",
+                                  style: textTheme.bodyMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: width,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.03,
+                        vertical: height * 0.01,
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    isLogin = true;
+                                  });
+                                },
+                                child: Container(
+                                  height: height * 0.03,
+                                  width: height * 0.03,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(color: grey),
+                                      color: white),
+                                  child: Icon(
+                                    Icons.circle,
+                                    size: height * 0.015,
+                                    color:
+                                        isLogin ? secondaryColor : transparent,
+                                  ),
+                                ),
+                              ),
+                              CommonFunctions.blankSpace(
+                                0,
+                                width * 0.02,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: "Sign In. ",
+                                      style: textTheme.bodyMedium!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "Already a Customer",
+                                      style: textTheme.bodyMedium,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          CommonFunctions.blankSpace(height * 0.01, 0),
+                          Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  showCountryPicker(
+                                      context: context,
+                                      onSelect: (val) {
+                                        setState(() {
+                                          currentCountryCode =
+                                              '+${val.phoneCode}';
+                                        });
+                                      });
+                                },
+                                child: Container(
+                                  height: height * 0.06,
+                                  width: width * 0.2,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: greyShade2,
+                                    border: Border.all(color: grey),
+                                    borderRadius: BorderRadius.circular(
+                                      5,
+                                    ),
+                                  ),
+                                  child: Text(
+                                    currentCountryCode,
+                                    style: textTheme.bodyMedium!.copyWith(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
